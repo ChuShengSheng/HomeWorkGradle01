42dbafa753bed696c759a3f20b7f6e620a2d96cc
Olga <151966653+ChuShengSheng@users.noreply.github.com>
2024-07-10 17:05:33 +0300

    Создан проект Gradle
    В проект добавлены зависимости com.google.guava:guava. 
    Создан модуль hw01-gradle. 
    В модуле сделан класс HelloGradle. 
    В этом классе сделан вызов метода из guava.
    Создан "толстый-jar".

---
+++ hw01-gradle/src/main/java/org/example/HelloGradle.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+
+import java.util.Set;
+
+public class HelloGradle {
+    public static void main(String[] args) {
+        Multimap<Integer, Set<String>> option4 = HashMultimap.create();
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Multimap<Integer, Set<String>> option4 = HashMultimap.create();
+    }
+}
